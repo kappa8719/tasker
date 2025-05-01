608419b3a2e021caf722ca71885a59469acb6017
Kappa <kappa@entropi.kr>
2025-05-02 02:10:49 +0900

    add navigator

---
+++ app/src/main/java/kr/entropi/tasker/ui/navigation/Navigator.kt
@@ -0,0 +1,111 @@
+package kr.entropi.tasker.ui.navigation
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Checklist
+import androidx.compose.material.icons.filled.Dns
+import androidx.compose.material3.DrawerState
+import androidx.compose.material3.DrawerValue
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.Icon
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
+import androidx.compose.material3.Text
+import androidx.compose.material3.rememberDrawerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.navigation.NavDestination.Companion.hasRoute
+import androidx.navigation.NavDestination.Companion.hierarchy
+import androidx.navigation.compose.currentBackStackEntryAsState
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import kr.entropi.tasker.navigation.LocalNavController
+import kr.entropi.tasker.navigation.MachineListEntry
+import kr.entropi.tasker.navigation.TaskListEntry
+
+private class LocalNavigatorContext(val drawerState: DrawerState)
+
+private val LocalNavigator =
+    compositionLocalOf<LocalNavigatorContext> { error("no LocalNavigator provided") }
+
+@Composable
+fun Navigator(content: @Composable () -> Unit) {
+    val drawerState = rememberDrawerState(DrawerValue.Closed)
+    val localNavigatorContext = LocalNavigatorContext(
+        drawerState = drawerState
+    )
+
+    CompositionLocalProvider(LocalNavigator provides localNavigatorContext) {
+        ModalNavigationDrawer(
+            drawerState = drawerState,
+            drawerContent = {
+                ModalDrawerSheet(drawerState = drawerState) {
+                    Text("Tasker", modifier = Modifier.padding(16.dp))
+                    HorizontalDivider()
+                    Column(
+                        Modifier.padding(8.dp),
+                        verticalArrangement = Arrangement.spacedBy(8.dp)
+                    ) {
+                        NavigatorItem(
+                            route = TaskListEntry,
+                            icon = { Icon(Icons.Default.Checklist, null) },
+                            text = { Text(text = "Tasks") }
+                        )
+                        NavigatorItem(
+                            route = MachineListEntry,
+                            icon = { Icon(Icons.Default.Dns, null) },
+                            text = { Text(text = "Machines") }
+                        )
+                    }
+                }
+            }
+        ) {
+            content()
+        }
+    }
+}
+
+@Composable
+private inline fun <reified T : Any> NavigatorItem(
+    route: T,
+    crossinline icon: @Composable () -> Unit = {},
+    crossinline text: @Composable () -> Unit = {}
+) {
+    val navController = LocalNavController.current
+    val currentDestination = navController.currentBackStackEntryAsState().value?.destination
+    val localNavigator = LocalNavigator.current
+    val coroutineScope = rememberCoroutineScope()
+
+    NavigationDrawerItem(
+        label = {
+            Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                icon()
+                text()
+            }
+        },
+        selected = currentDestination?.hierarchy?.any { it.hasRoute(T::class) } == true,
+        onClick = {
+            // navigate
+            if (currentDestination?.hierarchy?.first()?.hasRoute<T>() != true) {
+                navController.navigate(route)
+            }
+
+            // close drawer
+            coroutineScope.launch {
+                try {
+                    localNavigator.drawerState.close()
+                } catch (_: Throwable) {
+                    // the animation was interrupted
+                }
+            }
+        }
+    )
+}
