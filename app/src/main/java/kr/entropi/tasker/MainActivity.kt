c663451e0b8646857fcf0f0c0595b87ef3a94a52
Kappa <kappa@entropi.kr>
2025-05-02 02:07:32 +0900

    add surface with background color to sync with theme

--- app/src/main/java/kr/entropi/tasker/MainActivity.kt
+++ app/src/main/java/kr/entropi/tasker/MainActivity.kt
@@ -4,19 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.ui.platform.LocalContext
+import dagger.hilt.android.AndroidEntryPoint
 import kr.entropi.tasker.navigation.LocalNavHost
+import kr.entropi.tasker.ui.navigation.Navigator
 import kr.entropi.tasker.ui.theme.TaskerTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
             TaskerTheme {
-                CompositionLocalProvider(LocalContext provides this) {
-                    LocalNavHost()
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    CompositionLocalProvider(LocalContext provides this) {
+                        LocalNavHost { content ->
+                            Navigator { content() }
+                        }
+                    }
                 }
             }
         }
